93801c013201c13ae157bbcb7b4d195ef53a65ea
Rafayel114 <45944131+Rafayel114@users.noreply.github.com>
2020-01-28 14:54:33 +0300

    Update config.py

--- flask_app/test_app/config.py
+++ flask_app/test_app/config.py
@@ -1,7 +1,7 @@
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://rafayel114:1991114@localhost/test1'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://test111:pass111@localhost/test1'
 
     SECURITY_PASSWORD_SALT = 'salty_salt'
     SECURITY_PASSWORD_HASH = 'sha512_crypt'
